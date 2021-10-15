64eee4bf2d4daa16d552f138b10a2bc82aca869e
daidai53 <lifengting95@outlook.com>
2021-10-16 01:21:12 +0800

    image导入，开始使用，构建自己的Reader

---
+++ image-processor/imagereader/imagereader.go
@@ -0,0 +1,6 @@
+// Copyright@daidai53 2021
+package imagereader
+
+type ImageReader interface {
+	ReadByFile(in interface{})
+}
---
+++ main/application_test.go
@@ -0,0 +1,8 @@
+// Copyright@daidai53 2021
+package main
+
+import "testing"
+
+func Test_Application(t *testing.T) {
+
+}
