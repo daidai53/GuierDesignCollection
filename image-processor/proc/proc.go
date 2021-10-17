c5b029e79291703db74b8174d6c758afbf813692
daidai53 <lifengting95@outlook.com>
2021-10-17 23:32:50 +0800

    初步修改proc中图像处理的方法，不是很满意，待重构

--- image-processor/proc/proc.go
+++ image-processor/proc/proc.go
@@ -5,6 +5,7 @@ import (
 	"image"
 	"image/color"
 	"image/jpeg"
+	"io"
 	"log"
 	"os"
 )
@@ -16,20 +17,43 @@ func init() {
 }
 
 type ImageProc struct {
+	imageFile io.Reader
+	imageData image.Image
 }
 
-func (i ImageProc) ReadJpegAndConvertToGrayExample() {
-	imageFile, err := os.Open("input.jpg")
+func (i *ImageProc) Close() {
+}
+
+func (i *ImageProc) LoadImage(fileName string) error {
+	var err error
+	i.imageFile, err = os.OpenFile(fileName, os.O_RDWR, 0777)
 	if err != nil {
-		log.Default().Println("Error:open image file failed, exit.")
+		return err
+	}
+	return nil
+}
+
+func (i *ImageProc) DecodeImage() error {
+	var err error
+	i.imageData, err = jpeg.Decode(i.imageFile)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (i *ImageProc) ReadJpegAndConvertToGrayExample() {
+	err := i.LoadImage("input.jpg")
+	if err != nil {
+		log.Default().Println("Error:open image file failed, exit. Cause: %v.", err)
 		return
 	}
-	imageData, err := jpeg.Decode(imageFile)
+	err = i.DecodeImage()
 	if err != nil {
-		log.Default().Println("Error:decode image file failed, exit.")
+		log.Default().Println("Error:decode image file failed, exit. Cause: %v.", err)
 		return
 	}
-	imageFileOut, err := os.Open("output.jpg")
+	imageFileOut, err := os.OpenFile("output.jpg", os.O_RDWR, 0777)
 	if err != nil {
 		imageFileOut, err = os.Create("output.jpg")
 		if err != nil {
@@ -40,18 +64,22 @@ func (i ImageProc) ReadJpegAndConvertToGrayExample() {
 	option := jpeg.Options{
 		Quality: 100,
 	}
-	grayImage := image.NewGray(imageData.Bounds())
-	for i := 0; i < imageData.Bounds().Dx(); i++ {
-		for j := 0; j < imageData.Bounds().Dy(); j++ {
-			colorPix := imageData.At(i, j)
+	grayImage := image.NewGray(i.imageData.Bounds())
+	for index := 0; index < i.imageData.Bounds().Dx(); index++ {
+		for j := 0; j < i.imageData.Bounds().Dy(); j++ {
+			colorPix := i.imageData.At(index, j)
 			ycbcr, _ := colorPix.(color.YCbCr)
 			r, g, b := color.YCbCrToRGB(ycbcr.Y, ycbcr.Cb, ycbcr.Cr)
 			grayNum := float64(r)*0.299 + float64(g)*0.587 + float64(b)*0.114
 			gray := color.Gray{
 				Y: uint8(grayNum),
 			}
-			grayImage.SetGray(i, j, gray)
+			grayImage.SetGray(index, j, gray)
 		}
 	}
-	jpeg.Encode(imageFileOut, grayImage, &option)
+	err = jpeg.Encode(imageFileOut, grayImage, &option)
+	if err != nil {
+		log.Default().Printf("Error: encode failed, cause: %v", err)
+		return
+	}
 }
